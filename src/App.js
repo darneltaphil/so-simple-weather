e28e904daab69f8147f4329c5e73fa93aa2c924a
Akpadji <kofano.akpadji@vodafone.com>
2022-06-08 12:02:53 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import DayDisplay from "./components/DayDisplay";
 function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [weatherDailyData, setWeatherDailyData] = useState([]);
+  const [weatherTown, setWeatherTown] = useState("accra");
+
+  const getWeather = () => {
+    var requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://api.weatherbit.io/v2.0/forecast/daily?city=${weatherTown}&key=38ff7a5f9aa44a4fb8a39690b036d5cb`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setWeatherData(result);
+        setWeatherDailyData(result.data);
+      })
+      .catch((error) => console.log("error", error));
+  };
+
+  const handleSelectTown = (e) => {
+    e.preventDefault();
+    const town = e.target.value;
+    setWeatherTown(town);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [weatherTown]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ justifyContent: "center" }}>
+      <form>
+        <select
+          name="town"
+          onChange={handleSelectTown}
+          style={{ padding: "10px", width: "200px", alignItems: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <option value>Select town</option>
+          <option value="Accra">Accra</option>
+          <option value="London">London</option>
+          <option value="madrid">Madrid</option>
+        </select>
+      </form>
+      <div style={{ display: "table-row" }}>
+        <div
+          style={{
+            display: "table-cell",
+            width: "20%",
+            fontWeight: "bold",
+            fontSize: "14pt",
+            padding: "10px",
+          }}
+        >
+          {weatherData.city_name}
+        </div>
+        {weatherDailyData.slice(0, 5).map((value, i) => {
+          return (
+            <>
+              <DayDisplay key={i} dailyData={value} idx={i} />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/DayDisplay.js
@@ -0,0 +1,39 @@
+import React from "react";
+import moment from "moment";
+export default function DayDisplay({ dailyData, idx }) {
+  console.log("dailyData: ", new Date());
+
+  return (
+    <div style={{ display: "table-cell" }}>
+      <span style={{ color: "blue", fontWeight: "bold" }} id="day">
+        {moment(dailyData.datetime).format("dddd")}
+        {/* {dailyData?.datetime == moment(dailyData.dailyData).calendar()
+          ? "Today"
+          : dailyData?.datetime} */}
+      </span>
+      <br />
+      <div className="container">
+        <span className="" style={{ marginRight: "5px" }} id="icon">
+          <img
+            src={`https://www.weatherbit.io/static/img/icons/${dailyData.weather.icon}.png`}
+            width="25%"
+          />
+        </span>
+        <small style={{ marginRight: "5px" }} id="icon">
+          {dailyData?.weather?.description}
+        </small>
+      </div>
+      <br />
+      <small style={{}} id="max-temp">
+        Max Temp: {dailyData?.max_temp}&#176;
+      </small>
+      <br />
+      <small style={{ display: "table-row" }} id="min-temp">
+        Min Temp: {dailyData.min_temp}&#176;
+      </small>
+      <small style={{}} id="wind-speed">
+        {dailyData?.wind_spd} mph
+      </small>
+    </div>
+  );
+}
